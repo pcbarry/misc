dd2ef32c290939416a42dc89300cf54f39c71344
Patrick Barry <patrickbarry@Patricks-MacBook-Pro.local>
2020-12-03 22:19:55 -0500

    interpolated resummation

---
+++ dyres.py
@@ -0,0 +1,261 @@
+#!/usr/bin/env python
+import numpy as np
+#import pylab as py
+import math
+
+from mpmath           import fp
+from scipy.special    import gamma as Gamma
+from scipy.integrate  import quad
+from scipy.integrate  import fixed_quad
+from tools.tools      import load,save,checkdir#,load_config
+
+#from scipy.interpolate import CubicSpline as inter
+from scipy.interpolate import griddata
+
+from qcdlib import alphaS,aux,eweak
+from qcdlib import mellin
+from dyresmellin import DYMELLIN
+from qcdlib.pdfpionalt import PDF
+#from qcdlib.npdfalt import PDF as NPDF
+from qcdlib.wpdfalt import PDF as WPDF
+from tools.config import conf,load_config
+
+class DYRES:
+
+    def __init__(self):
+
+        self.setup_constants()
+        self.storage={}
+        self.data={}
+        self.storage_match={}
+        self.alphaS=conf['alphaS']
+
+        self.mell={}
+        self.mell[+1]=conf['dymell1']
+        self.mell[+2]=conf['dymell2']
+        self.mell[+3]=conf['dymell3']
+
+        #self.pdfA={}
+        #self.pdfA[+1]=conf['resummed pdf-pion 1']
+        #self.pdfA[+2]=conf['resummed pdf-pion 2']
+        #self.pdfA[+3]=conf['resummed pdf-pion 3']
+        self.pdfA=conf['pdf-pion']
+
+        #self.pdfB={}
+        #self.pdfB[-1]=conf['resummed W pdf 1']
+        #self.pdfB[-2]=conf['resummed W pdf 2']
+        #self.pdfB[-3]=conf['resummed W pdf 3']
+        self.pdfB=conf['pdf-W']
+
+    def setup_constants(self):
+        self.CA=3.0
+        self.CF=4.0/3.0
+        self.euler=fp.euler
+
+        self.eU2=4.0/9.0
+        self.eD2=1.0/9.0
+
+    def get_lum(self,fA,fB,ic,muR2,muF2):
+        Nf=self.alphaS.get_Nf(muR2)
+
+        out= self.eU2*(fA.res_storage[(ic,muF2)]['u']  * fB.res_storage[(-ic,muF2)]['ub']\
+                      +fA.res_storage[(ic,muF2)]['ub'] * fB.res_storage[(-ic,muF2)]['u'])\
+            +self.eD2*(fA.res_storage[(ic,muF2)]['d']  * fB.res_storage[(-ic,muF2)]['db']\
+                      +fA.res_storage[(ic,muF2)]['db'] * fB.res_storage[(-ic,muF2)]['d'])\
+            +self.eD2*(fA.res_storage[(ic,muF2)]['s']  * fB.res_storage[(-ic,muF2)]['sb']\
+                      +fA.res_storage[(ic,muF2)]['sb'] * fB.res_storage[(-ic,muF2)]['s'])
+        if Nf>3: out+=self.eU2*(fA.res_storage[(ic,muF2)]['c']  * fB.res_storage[(-ic,muF2)]['cb']\
+                      +fA.res_storage[(ic,muF2)]['cb'] * fB.res_storage[(-ic,muF2)]['c'])
+        if Nf>4: out+=self.eD2*(fA.res_storage[(ic,muF2)]['b']  * fB.res_storage[(-ic,muF2)]['bb']\
+                      +fA.res_storage[(ic,muF2)]['bb'] * fB.res_storage[(-ic,muF2)]['b'])
+
+        return out
+
+    def _get_omega(self,N,muR2,muF2):
+        CA,CF,euler=self.CA,self.CF,self.euler
+
+        Nf=self.alphaS.get_Nf(muR2)
+        aS=self.alphaS.get_alphaS(muR2)
+        Nbar=N*np.exp(euler)
+
+        b0=1.0/12.0/np.pi*(11*CA-2*Nf)
+        b1=1.0/24.0/np.pi**2*(17*CA**2-5*CA*Nf-3*CF*Nf)
+        lam=b0*aS*np.log(Nbar)
+        Aq1=CF
+        Aq2=1.0/2.0*CF*(CA*(67.0/18.0-np.pi**2/6.0)-5.0/9.0*Nf)
+
+        Cq=aS/np.pi*CF*(-4+2*np.pi**2/3.0)
+        h1=Aq1/2.0/np.pi/b0/lam*(2*lam+(1-2*lam)*np.log(1-2*lam))
+        h2=(np.pi*Aq1*b1-b0*Aq2)*(2*lam+np.log(1-2*lam))/2.0/np.pi**2/b0**3\
+           +Aq1*b1/4.0/np.pi/b0**3*np.log(1-2*lam)**2
+
+        #match=1.0+aS/6.0/np.pi*(CF*(-24+4*np.pi**2)+12*Aq1*np.log(Nbar)**2)
+
+        if muF2!=muR2:
+            Cq+=aS/np.pi*CF*3.0/2.0*np.log(muR2/muF2)
+            h2+=Aq1/2.0/np.pi/b0*np.log(1-2*lam)*np.log(muR2/muF2)
+            #match+=aS/np.pi*(CF*3.0/2.0*np.log(muR2/muF2)-2*Aq1*np.log(Nbar)*np.log(muR2/muF2))
+
+        if conf['Cq']=='exponential':
+            logomega=Cq+2*h1*np.log(Nbar)+2*h2
+            return np.exp(logomega)#-match
+        elif conf['Cq']=='prefactor':
+            logomega=2*h1*np.log(Nbar)+2*h2
+            return (1+Cq)*np.exp(logomega)#-match
+
+    def get_omega(self,muR2,muF2):
+        if (muR2,muF2) not in self.storage: 
+            self.storage[(muR2,muF2)]={}
+            if conf['MP method']=='cosine':
+              self.storage[(muR2,muF2)][1]=0.5*(self._get_omega(self.mell[1].Np,muR2,muF2)\
+                                               +self._get_omega(self.mell[1].Nm,muR2,muF2))
+              self.storage[(muR2,muF2)][2]=0.5*(self._get_omega(self.mell[2].Np,muR2,muF2)\
+                                               +self._get_omega(self.mell[2].Nm,muR2,muF2))
+              self.storage[(muR2,muF2)][3]=0.5*(self._get_omega(self.mell[3].Np,muR2,muF2)\
+                                               +self._get_omega(self.mell[3].Nm,muR2,muF2))
+            elif conf['MP method']=='expansion':
+              self.storage[(muR2,muF2)][1]=self._get_omega(self.mell[1].N,muR2,muF2)
+              self.storage[(muR2,muF2)][2]=self._get_omega(self.mell[2].N,muR2,muF2)
+              self.storage[(muR2,muF2)][3]=self._get_omega(self.mell[3].N,muR2,muF2)
+            elif conf['MP method']=='exact':
+              self.storage[(muR2,muF2)][1]=self._get_omega_exact(self.mell[1].Np,self.mell[1].Nm,muR2,muF2)
+              self.storage[(muR2,muF2)][2]=self._get_omega_exact(self.mell[2].Np,self.mell[2].Nm,muR2,muF2)
+              self.storage[(muR2,muF2)][3]=self._get_omega_exact(self.mell[3].Np,self.mell[3].Nm,muR2,muF2)
+        return self.storage[(muR2,muF2)]
+
+    def _get_match(self,N,muR2,muF2):
+        CA,CF,euler=self.CA,self.CF,self.euler
+
+        Nf=self.alphaS.get_Nf(muR2)
+        aS=self.alphaS.get_alphaS(muR2)
+        Nbar=N*np.exp(euler)
+
+        Aq1=CF
+        match=1.0+aS/6.0/np.pi*(CF*(-24+4*np.pi**2)+12*Aq1*np.log(Nbar)**2)
+
+        if muF2!=muR2:
+            match+=aS/np.pi*(CF*3.0/2.0*np.log(muR2/muF2)-2*Aq1*np.log(Nbar)*np.log(muR2/muF2))
+
+        return match
+
+    def get_match(self,muR2,muF2):
+        if (muR2,muF2) not in self.storage_match:
+            self.storage_match[(muR2,muF2)]={}
+            if conf['MP method']=='cosine':
+              self.storage_match[(muR2,muF2)][1]=0.5*(self._get_match(self.mell[1].Np,muR2,muF2)\
+                                                     +self._get_match(self.mell[1].Nm,muR2,muF2))
+              self.storage_match[(muR2,muF2)][2]=0.5*(self._get_match(self.mell[2].Np,muR2,muF2)\
+                                                     +self._get_match(self.mell[2].Nm,muR2,muF2))
+              self.storage_match[(muR2,muF2)][3]=0.5*(self._get_match(self.mell[3].Np,muR2,muF2)\
+                                                     +self._get_match(self.mell[3].Nm,muR2,muF2))
+            elif conf['MP method']=='expansion':
+              self.storage_match[(muR2,muF2)][1]=self._get_match(self.mell[1].N,muR2,muF2)
+              self.storage_match[(muR2,muF2)][2]=self._get_match(self.mell[2].N,muR2,muF2)
+              self.storage_match[(muR2,muF2)][3]=self._get_match(self.mell[3].N,muR2,muF2)
+        return self.storage_match[(muR2,muF2)]
+
+    def _get_xsec(self,tau,muR2,muF2,savedir=False,savename=None,verb=False):
+        if verb: print 'computing omega'
+        omega=self.get_omega(muR2,muF2)
+        match=self.get_match(muR2,muF2)
+        if verb: print 'evolving pdfs'
+        #for _ in self.pdfA:  self.pdfA[_].evolve(muF2) 
+        #for _ in self.pdfB:  self.pdfB[_].evolve(muF2)
+        self.pdfA.evolve(muF2)
+        self.pdfB.evolve(muF2)
+        if savedir: 
+            data=self.data
+            data['muF2=%.2f'%muF2]={}
+            data['muF2=%.2f'%muF2]['NM']={}
+            data['muF2=%.2f'%muF2]['M']={}
+            data['muF2=%.2f'%muF2]['match']={}
+
+
+        if verb: print 'integrating over N'
+        sigM=0
+        matchM=0
+        for ic in [1,2,3]:
+            lum=self.get_lum(self.pdfA,self.pdfB,ic,muR2,muF2)
+
+            sigNM = lum*omega[ic]
+            matchNM=lum*match[ic]
+            if ic==1:
+                sigM-=self.mell[ic].invert(tau,sigNM)
+                matchM-=self.mell[ic].invert(tau,matchNM)
+                if savedir:
+                    data['muF2=%.2f'%muF2]['NM'][ic]=-sigNM
+                    data['muF2=%.2f'%muF2]['M'][ic]=-self.mell[ic].invert(tau,sigNM)
+                    data['muF2=%.2f'%muF2]['match'][ic]=-self.mell[ic].invert(tau,matchNM)
+
+            else:
+                sigM+=self.mell[ic].invert(tau,sigNM)
+                matchM+=self.mell[ic].invert(tau,matchNM)
+                if savedir:
+                    data['muF2=%.2f'%muF2]['NM'][ic]=sigNM
+                    data['muF2=%.2f'%muF2]['M'][ic]=self.mell[ic].invert(tau,sigNM)
+                    data['muF2=%.2f'%muF2]['match'][ic]=self.mell[ic].invert(tau,matchNM)
+
+        totsig=sigM-matchM
+
+        if verb: print 'integrating over M'
+        #return sig
+        if savedir:
+            self.data=data
+            checkdir('%s'%savedir)
+            save(self.data,'%s/%s'%(savedir,savename))
+        return totsig
+
+    def interpolating_function(self,tau,muR2,muF2,M):
+
+      aEM=conf['eweak'].get_alpha(muR2)
+      S=muF2/tau
+      prefactor=4*np.pi*aEM**2/9.0/S/muF2
+
+      xsec=self._get_xsec(tau,muR2,muF2)
+      raw=prefactor*xsec
+
+      return griddata(conf['dymell1'].M,raw,M,fill_value=0,method='cubic')
+
+
+    def get_xsec(self,S,Y,muR2,muF2):
+      tau=muF2/S
+      func=lambda M: np.exp(-1j*M*Y)/2./np.pi*self.interpolating_function(tau,muR2,muF2,M)
+      return 2*np.real(fixed_quad(func,0,30,n=200)[0])
+
+
+if __name__=="__main__":
+
+    conf['order']='NLO'
+    conf['Q20'] = 1.0 
+    conf['aux']=aux.AUX()
+    conf['dglap mode']='truncated'
+    conf['alphaS']=alphaS.ALPHAS()
+    conf['Z']=74.
+    conf['A']=184.
+    conf['eweak']=eweak.EWEAK()
+    conf['mellin-pion']=mellin.MELLIN(npts=8,extended=True)
+    conf['mellin']=mellin.MELLIN()
+    conf['MP method']='cosine'
+
+    conf['dymell1']=DYMELLIN(contour=1)
+    conf['dymell2']=DYMELLIN(contour=2)
+    conf['dymell3']=DYMELLIN(contour=3)
+
+    conf['pdf-pion']=PDF()
+
+    conf['pdf-W']=WPDF()
+
+    dyres=DYRES()
+
+    tau=0.3345
+    muR2=53.05
+    muF2=53.05
+    S=muF2/tau
+    Y=0.074669
+    print dyres.get_interpolated(S,Y,muR2,muF2)
+    print dyres.get_interpolated(S,0.1,muR2,muF2)
+    #dyres.get_xsec(tau,muR2,muF2,verb=True)
+
+
+
+
---
+++ pdfpionalt.py
@@ -0,0 +1,356 @@
+#!/bin/env python
+import sys
+from scipy.special import gamma
+import numpy as np
+from qcdlib.alphaS import ALPHAS
+from qcdlib.aux import AUX
+from qcdlib.dglap import DGLAP
+from qcdlib.kernels import KERNELS
+from qcdlib.mellin import MELLIN
+from tools.config import conf
+from obslib.dy.dyresmellin import DYMELLIN
+from scipy.integrate import quad
+import copy
+
+class PDF: 
+
+  def __init__(self,mellin=None):
+    """
+    This PDF should be used for resummation purposes.
+    Assumes pion BEAM incident on target.
+    """
+
+    self.spl='upol'
+    self.Q20=conf['Q20']
+    self.mc2=conf['aux'].mc2
+    self.mb2=conf['aux'].mb2
+
+    mell=copy.deepcopy(conf['mellin-pion'])
+    mell1=copy.deepcopy(conf['dymell1'])
+    mell2=copy.deepcopy(conf['dymell2'])
+    mell3=copy.deepcopy(conf['dymell3'])
+
+    self.Nsize=mell.N.size #--from M-independent contour
+    self.resN1size=mell1.Np.size #--one contour for +M, full for -M
+    self.resN2size=mell2.Np.size #--improvements based on M values should be made
+    self.resN3size=mell3.Np.size #--one contour for -M, full for +M
+
+    self.M=conf['dymell1'].M
+
+    mell.N=np.append(mell.N,mell1.Np)
+    mell.N=np.append(mell.N,mell2.Np)
+    mell.N=np.append(mell.N,mell3.Np)
+
+    print 'initializing the kernel'
+    self.kernel=KERNELS(mell,self.spl)
+    print 'initializing dglap'
+    self.dglap=DGLAP(mell,conf['alphaS'],self.kernel,conf['dglap mode'],conf['order'])
+    self.mellin=mell
+
+    self.set_params()
+    self.setup()
+
+  def set_params(self):
+    """
+    f(x) = norm * x**a * (1-x)**b * (1+c*x**0.5+d*x)
+    """
+    params={}
+
+    params['g1'] =np.array([0,-0.5,3,0,0])
+    params['g2'] =np.array([0,-0.5,3,0,0])
+    params['g3'] =np.array([0,-0.5,3,0,0])
+    params['ubv1']=np.array([1, 0.5,3,0,0])
+    params['ubv2']=np.array([0, 0.5,3,0,0])
+    params['ubv3']=np.array([0, 0.5,3,0,0])
+    params['dv1']=np.array([1, 0.5,3,0,0])
+    params['dv2']=np.array([0, 0.5,3,0,0])
+    params['dv3']=np.array([0, 0.5,3,0,0])
+    params['u1']=np.array([1,-0.5,3,0,0])
+    params['u2']=np.array([0,-0.5,3,0,0])
+    params['u3']=np.array([0,-0.5,3,0,0])
+    params['db1']=np.array([1,-0.5,3,0,0])
+    params['db2']=np.array([0,-0.5,3,0,0])
+    params['db3']=np.array([0,-0.5,3,0,0])
+    params['s1'] =np.array([0,-0.5,3,0,0])
+    params['s2'] =np.array([0,-0.5,3,0,0])
+    params['s3'] =np.array([0,-0.5,3,0,0])
+    params['sb1']=np.array([0,-0.5,3,0,0])
+    params['sb2']=np.array([0,-0.5,3,0,0])
+    params['sb3']=np.array([0,-0.5,3,0,0])
+
+    self.params=params
+
+  def set_sumrules(self):
+
+
+    #--valence
+    self.params['ubv1'][0]=1    
+    self.params['ubv1'][0]=(1-self.get_moments('ubv2',1)-self.get_moments('ubv3',1))/self.get_moments('ubv1',1)
+
+    self.params['dv1'][0]=1    
+    self.params['dv1'][0]=(1-self.get_moments('dv2',1)-self.get_moments('dv3',1))/self.get_moments('dv1',1)
+
+    #--msr
+    u23   =                            self.get_moments('u2',2)+self.get_moments('u3',2)
+    ubv   = self.get_moments('ubv1',2)+self.get_moments('ubv2',2)+self.get_moments('ubv3',2)
+    up23  = 2*u23 + ubv
+
+    db23 =                           self.get_moments('db2',2)+self.get_moments('db3',2)
+    dv   = self.get_moments('dv1',2)+self.get_moments('dv2',2)+self.get_moments('dv3',2)
+    dp23 = dv + 2*db23
+
+    s23  =                           self.get_moments('s2',2)+self.get_moments('s3',2)
+    sb23 =                           self.get_moments('sb2',2)+self.get_moments('sb3',2)
+    sp23 = s23+sb23   
+
+    g    = self.get_moments('g1',2)+self.get_moments('g2',2)+self.get_moments('g3',2)
+
+    self.params['u1'][0]=6    
+    self.params['u1'][0]=(1-up23-dp23-sp23-g)/self.get_moments('u1',2)
+
+    self.params['db1'][0]=self.params['u1'][0]
+    self.params['s1'][0] =self.params['u1'][0]
+    self.params['sb1'][0]=self.params['u1'][0]
+
+    u  = self.get_moments('u1',2)+u23
+    db = self.get_moments('db1',2)+db23
+    s  = self.get_moments('s1',2)+s23
+    sb = self.get_moments('sb1',2)+sb23
+
+    up = 2*u + ubv
+    dp = dv + 2*db
+    sp = s + sb
+
+    msr=g+up+dp+sp
+
+    #--share
+    self.sr={}
+    self.sr['msr']=msr
+    self.sr['ubv(1)'] = self.get_moments('ubv1',1)+self.get_moments('ubv2',1)+self.get_moments('ubv3',1)
+    self.sr['dv(1)']  = self.get_moments('dv1',1)+self.get_moments('dv2',1)+self.get_moments('dv3',1)
+    self.sr['g(2)']   = g
+    self.sr['sea(2)'] = 6*u 
+    self.sr['valence(2)']=2*ubv
+
+  def set_moms(self):
+
+    moms={}
+
+    moms['g']=self.get_moments('g1')+self.get_moments('g2')+self.get_moments('g3')
+
+    u   = self.get_moments('u1')+self.get_moments('u2')+self.get_moments('u3')
+    ubv = self.get_moments('ubv1')+self.get_moments('ubv2')+self.get_moments('ubv3')
+    ub  = ubv+u
+    moms['up']=u + ub
+
+    db = self.get_moments('db1')+self.get_moments('db2')+self.get_moments('db3')
+    dv = self.get_moments('dv1')+self.get_moments('dv2')+self.get_moments('dv3')
+    d  = dv+db
+    moms['dp']=d+db
+
+    s  = self.get_moments('s1') +self.get_moments('s2')+self.get_moments('s3')
+    sb = self.get_moments('sb1')+self.get_moments('sb2')+self.get_moments('sb3')
+    moms['sp']=s+sb   
+
+    moms['um']=u-ub
+    moms['dm']=d-db
+    moms['sm']=s-sb
+    self.moms0=moms
+    self.get_BC(moms)
+
+  def setup(self):
+  
+    self.set_sumrules()
+    self.set_moms()
+
+    # we will store all Q2 values that has been precalc
+    self.storage={}
+    self.res_storage={}
+
+  def beta(self,a,b):
+    return gamma(a)*gamma(b)/gamma(a+b)
+
+  def get_moments(self,flav,N=None):
+    """
+    if N==None: then parametrization is to be use to compute moments along mellin contour
+    else the Nth moment is returned
+    for valence: N x**a*(1-x)**b*(1+c*x**d)
+    """
+    if N==None: N=self.mellin.N
+    M,a,b,c,d=self.params[flav]
+    if 'v' in flav:
+        norm=self.beta(2+a,b+1)+c*self.beta(2+a+d,b+1)
+        mom=self.beta(N+a,b+1)+c*self.beta(N+a+d,b+1)
+    else:
+        norm=self.beta(2+a,b+1)+c*self.beta(2+a+0.5,b+1)+d*self.beta(2+a+1.0,b+1)
+        mom=self.beta(N+a,b+1)+c*self.beta(N+a+0.5,b+1)+d*self.beta(N+a+1.0,b+1)
+    return M*mom/norm
+
+  def _get_BC(self,g,up,um,dp,dm,sp,sm,cp,cm,bp,bm,tp,tm):
+    N=self.mellin.N
+    
+    # flav composition
+    vm,vp={},{}
+    vm[35]= bm + cm + dm + sm - 5*tm + um
+    vm[24]= -4*bm + cm + dm + sm + um 
+    vm[15]= -3*cm + dm + sm + um
+    vm[8] = dm - 2*sp + 2*(-sm + sp) + um
+    vm[3] = -dm + um
+    vm[0] = np.zeros(N.size,dtype=complex)
+    vp[0] = np.zeros(N.size,dtype=complex)
+    vp[3] = -dp + up
+    vp[8] = dp - 2*sp + up 
+    vp[15]= -3*cp + dp + sp + up 
+    vp[24]= -4*bp + cp + dp + sp + up 
+    vp[35]= bp + cp + dp + sp - 5*tp + up 
+    qs    = bp + cp + dp + sp + tp + up 
+    qv    = bm + cm + dm + sm + tm + um 
+    q     = np.zeros((2,N.size),dtype=complex)
+    q[0]=np.copy(qs)
+    q[1]=np.copy(g)
+
+    BC={}
+    BC['vm']=vm 
+    BC['vp']=vp 
+    BC['qv']=qv
+    BC['q'] =q
+    return BC
+
+  def get_state(self):
+      return (self.BC3,self.BC4,self.BC5)
+  
+  def set_state(self,state):
+      self.BC3, self.BC4, self.BC5 = state[:]
+      self.storage = {}
+  
+  def get_BC(self,moms):
+
+    N=self.mellin.N
+    zero=np.zeros(N.size,dtype=complex)
+
+    ###############################################
+    # BC for Nf=3
+    g   = moms['g']
+    up  = moms['up']
+    um  = moms['um']
+    dp  = moms['dp']
+    dm  = moms['dm']
+    sp  = moms['sp']
+    sm  = moms['sm']
+    cp  = zero
+    cm  = zero
+    bp  = zero
+    bm  = zero
+    self.BC3=self._get_BC(g,up,um,dp,dm,sp,sm,zero,zero,zero,zero,zero,zero)
+
+    ###############################################
+    # BC for Nf=4
+    BC4=self.dglap.evolve(self.BC3,self.Q20,self.mc2,3)
+    g =BC4['g']
+    up=BC4['up']
+    dp=BC4['dp']
+    sp=BC4['sp']
+    cp=BC4['cp']
+    bp=BC4['bp']
+    tp=BC4['tp']
+    um=BC4['um']
+    dm=BC4['dm']
+    sm=BC4['sm']
+    cm=BC4['cm']
+    bm=BC4['bm']
+    tm=BC4['tm']
+    self.BC4=self._get_BC(g,up,um,dp,dm,sp,sm,cp,cm,bp,bm,tp,tm)
+
+    ###############################################
+    # BC for Nf=5
+    BC5=self.dglap.evolve(self.BC4,self.mc2,self.mb2,4)
+    g =BC5['g']
+    up=BC5['up']
+    dp=BC5['dp']
+    sp=BC5['sp']
+    cp=BC5['cp']
+    bp=BC5['bp']
+    tp=BC5['tp']
+    um=BC5['um']
+    dm=BC5['dm']
+    sm=BC5['sm']
+    cm=BC5['cm']
+    bm=BC5['bm']
+    tm=BC5['tm']
+    self.BC5=self._get_BC(g,up,um,dp,dm,sp,sm,cp,cm,bp,bm,tp,tm)
+
+  def evolve(self,Q2):
+
+    if Q2 not in self.storage:
+        self.storage[Q2]={}
+        self.res_storage[(1,Q2)]={}
+        self.res_storage[(2,Q2)]={}
+        self.res_storage[(3,Q2)]={}
+        if self.mb2<Q2: 
+            moms=self.dglap.evolve(self.BC5,self.mb2,Q2,5)
+        elif self.mc2<=Q2 and Q2<=self.mb2: 
+            moms=self.dglap.evolve(self.BC4,self.mc2,Q2,4)
+        elif Q2<self.mc2: 
+            moms=self.dglap.evolve(self.BC3,self.Q20,Q2,3)
+
+        i0=self.Nsize
+        i1=i0+self.resN1size
+        i2=i1+self.resN2size
+        flav=['g','u','ub','d','db','s','sb','c','cb','b','bb','qp','qm']
+        for f in flav:
+            if f=='qp' or f=='qm':
+                store=[]
+                for j in range(len(moms[f])):
+                    store.append(moms[f][j][:i0])
+                self.storage[Q2][f]=store
+            else:
+                self.storage[Q2][f]=moms[f][:i0]
+                self.res_storage[(1,Q2)][f]=moms[f][i0:i1]
+                self.res_storage[(2,Q2)][f]=moms[f][i1:i2]
+                self.res_storage[(3,Q2)][f]=moms[f][i2:]
+
+  def get_xF(self,x,Q2,flav,evolve=True):
+    if evolve: self.evolve(Q2)
+    return x*conf['mellin-pion'].invert(x,self.storage[Q2][flav])
+    #return x*self.mellin.invert(x,self.storage[Q2][flav])
+
+  def get_xF0(self,x,flav,evolve=True):
+    if flav=='um': mom=self.moms0['um']
+    elif flav=='dm': mom=self.moms0['dm']
+    elif flav=='sm': mom=self.moms0['sm']
+    return x*conf['mellin-pion'].invert(x,mom)
+    #return x*self.mellin.invert(x,mom)
+
+  def get_second_moment(self,flav,Q2):
+    self.evolve(Q2)
+    if flav=='g': 
+      f=lambda x: self.get_xF(x,Q2,'g',evolve=False)
+    if flav=='sea': 
+      f=lambda x: 2*self.get_xF(x,Q2,'u',evolve=False)\
+                 +2*self.get_xF(x,Q2,'db',evolve=False)\
+                 +self.get_xF(x,Q2,'s',evolve=False)\
+                 +self.get_xF(x,Q2,'sb',evolve=False)
+    if flav=='valence':
+      f=lambda x: self.get_xF(x,Q2,'ub',evolve=False)-self.get_xF(x,Q2,'u',evolve=False)\
+                 +self.get_xF(x,Q2,'d',evolve=False) -self.get_xF(x,Q2,'db',evolve=False)
+    return quad(f,0,1)[0]
+
+  def get_pdfs(self,x,Q2,evolve=True):
+    xf=[]
+    for f in ['g','u','ub','d','db','s','sb','c','cb','b','bb']:
+      xf.append(self.get_xF(x,Q2,f))
+    return xf/x
+
+if __name__=='__main__':
+
+  conf['order']='NLO'
+  conf['Q20'] = 1.0
+  conf['aux']=AUX()
+  conf['mellin']=MELLIN(npts=8)
+  conf['alphaS']=ALPHAS()
+  pdf=PDF('upol')
+  ppdf=PDF('pol')
+  print pdf.get_xF(0.5,10.,'u')
+  print ppdf.get_xF(0.5,10.,'u')
+
+
